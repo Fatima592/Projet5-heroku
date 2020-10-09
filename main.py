45c730b37e853ff39c1082a25b264915009edf5c
fatima mdoihoma <mdoihoma.fatima@icloud.com>
2020-10-09 09:15:43 +0200

    derniere modif

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 import uvicorn
 import sqlite3
-
-
+import json
+ 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+ 
 conn = sqlite3.connect('cars.db')
+
+conn.row_factory = dict_factory
+
 c = conn.cursor()
 
 app = FastAPI(title="REST API using electric cars")
